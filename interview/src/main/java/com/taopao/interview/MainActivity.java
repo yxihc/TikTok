57133c437f9bac9f0302cf3ee66bd6ccf1015fab
shenkun <825210484@qq.com>
2018-08-30 16:56:15 +0800

    新增注解

--- interview/src/main/java/com/taopao/interview/MainActivity.java
+++ interview/src/main/java/com/taopao/interview/MainActivity.java
@@ -2,7 +2,6 @@ package com.taopao.interview;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.taopao.interview.basics.BasicsActivity;
