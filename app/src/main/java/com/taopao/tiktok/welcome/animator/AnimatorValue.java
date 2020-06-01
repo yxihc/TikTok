9d26618c51e8146a7224e7cf0ec7023bcf7a50a8
shenkun <825210484@qq.com>
2020-06-01 16:24:21 +0800

    初始化

--- app/src/main/java/com/taopao/tiktok/welcome/animator/AnimatorValue.java
+++
@@ -1,9 +0,0 @@
-package com.taopao.tiktok.welcome.animator;
-
-import android.animation.ObjectAnimator;
-
-public interface AnimatorValue {
-	public abstract ObjectAnimator getAnimator();
-	public abstract ObjectAnimator getBeforeAnimator();
-	public abstract void before(AnimatorValue beforeAnimatorValue);
-}
